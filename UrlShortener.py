1000b63d1c7a7a885cca66bc0b0a750019294020
Hipolit <76755841+KucziSa@users.noreply.github.com>
2022-08-13 23:41:20 +0200

    Add files via upload

---
+++ UrlShortener.py
@@ -0,0 +1,19 @@
+import requests,os,sys
+cyan= '\033[36m'
+bold= '\033[1m'
+end= '\033[0m'
+
+def GenTiny():
+	 
+    print("""{1}{2}{1}
+╔═╗┌─┐┌┐┌╔╦╗┬┌┐┌┬ ┬
+║ ╦├┤ │││ ║ ││││└┬┘
+╚═╝└─┘┘└┘ ╩ ┴┘└┘ ┴  
+  {0}{1}Kuczi${2} 
+""".format(end,bold,cyan))
+GenTiny()
+url = input("\033[0m\033[1m\033[36m~/URL# \033[0m\033[1m")
+tiny = 'http://tinyurl.com/api-create.php?url='+url
+info = requests.get(tiny)
+print("\033[0m\033[1m\033[36m~/Link# \033[0m\033[1m"+info.text)
+
